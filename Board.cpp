cef53b40995439fe3c5eb69a18d627de0cc2ad0f
Ellis McDougald <ecmcdougald@gmail.com>
2023-12-31 14:47:36 -0700

    Adds bitboard printers and initializes board to starting position

--- Board.cpp
+++ Board.cpp
@@ -5,6 +5,14 @@
 #pragma once
 
 #include"Board.hpp"
+#include"startingpositions.hpp"
+
+// Constructors:
+Board::Board(bool empty) : white_bitboards(), black_bitboards() {
+  if(!empty) {
+    initialize_starting_position();
+  }
+}
 
 // Getters:
 bitboard Board::get_piece_positions(piece piece, bool white) {
@@ -12,6 +20,15 @@ bitboard Board::get_piece_positions(piece piece, bool white) {
     return white_bitboards[piece];
   } else {
     return black_bitboards[piece];
+  }
+}
+
+// Printers:
+void Board::print_piece_positions(piece p, bool white) {
+  if(white) {
+    print_bitboard(white_bitboards[p]);
+  } else {
+    print_bitboard(black_bitboards[p]);
   }
 }
 
@@ -58,5 +75,33 @@ void Board::undo_move(Move *move) {
     set_piece_position(move->get_start_position(),
 		       move->get_capture_piece(),
 		       ~move->get_white());
+  }
+}
+
+// Initializers
+void Board::initialize_starting_position(void) {
+  std::cout << "run\n";
+  set_piece_position(KING_START_POSITION, KING, true);
+  set_piece_position(QUEEN_START_POSITION, QUEEN, true);
+  set_piece_position(BISHOP_START_POSITION, BISHOP, true);
+  set_piece_position(KNIGHT_START_POSITION, KNIGHT, true);
+  set_piece_position(ROOK_START_POSITION, ROOK, true);
+  set_piece_position(PAWN_START_POSITION, PAWN, true);
+
+  set_piece_position(KING_START_POSITION, KING, false);
+  set_piece_position(QUEEN_START_POSITION, QUEEN, false);
+  set_piece_position(BISHOP_START_POSITION, BISHOP, false);
+  set_piece_position(KNIGHT_START_POSITION, KNIGHT, false);
+  set_piece_position(ROOK_START_POSITION, ROOK, false);
+  set_piece_position(PAWN_START_POSITION, PAWN, false);
+}
+
+// Print
+void Board::print_bitboard(bitboard bb) {
+  bitboard mask = (bitboard) 1 << 63;
+  for(int i = 0; i < 64; i++) {
+    std::cout << (mask & bb ? 1 : 0);
+    mask >>= 1;
+    if((i + 1) % 8 == 0) std::cout << "\n";
   }
 }
--- Board.hpp
+++ Board.hpp
@@ -4,20 +4,30 @@
 
 class Board {
 public:
-  Board();
+  // Constructors:
+  Board(bool empty);
 
-  // Getters
+  // Getters:
   bitboard get_piece_positions(piece p, bool white);
 
-  // Board management
+  // Printers:
+  void print_piece_positions(piece p, bool white);
+
+  // Board management:
   void set_piece_position(bitboard position, piece p, bool white);
   void clear_piece_position(bitboard position, piece p, bool white);
 
-  // Moves
+  // Moves:
   void execute_move(Move *move);
   void undo_move(Move *move);
   
 private:
   bitboard white_bitboards[6];
   bitboard black_bitboards[6];
+
+  // Initializers
+  void initialize_starting_position(void);
+
+  // Print
+  void print_bitboard(bitboard bitboard);
 };
