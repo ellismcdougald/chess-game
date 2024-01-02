762a1cf5cb33831cac14b6b94f3e9e95a60913ae
Ellis McDougald <ecmcdougald@gmail.com>
2024-01-02 13:54:54 -0700

    Adds knight and pawn lookup tables

--- LookupTables.cpp
+++ LookupTables.cpp
@@ -9,6 +9,10 @@
 // Constructors:
 LookupTables::LookupTables(void) {
   initialize_king_move_lookup_table();
+  initialize_knight_move_lookup_table();
+  initialize_pawn_single_push_lookup_table();
+  initialize_pawn_double_push_lookup_table();
+  initialize_pawn_attack_lookup_table();
 }
 
 // Getters:
@@ -16,13 +20,56 @@ bitboard LookupTables::get_king_move_bitboard(bitboard position) {
   return king_move_lookup_table[position];
 }
 
+bitboard LookupTables::get_knight_move_bitboard(bitboard position) {
+  return knight_move_lookup_table[position];
+}
+
+bitboard LookupTables::get_pawn_single_push_bitboard(bitboard position) {
+  return pawn_single_push_lookup_table[position];
+}
+
+bitboard LookupTables::get_pawn_double_push_bitboard(bitboard position) {
+  return pawn_double_push_lookup_table[position];
+}
+
+bitboard LookupTables::get_pawn_attack_bitboard(bitboard position) {
+  return pawn_attack_lookup_table[position];
+}
+
 // Initializers:
 void LookupTables::initialize_king_move_lookup_table(void) {
-  bitboard result;
   for(bitboard position = 1; position > 0; position <<= 1) {
     king_move_lookup_table[position] = NORTH(position) | SOUTH(position) | WEST(position) |
       EAST(position) | NORTHWEST(position) | NORTHEAST(position) | SOUTHWEST(position) |
       SOUTHEAST(position);
+  }
+}
+
+void LookupTables::initialize_knight_move_lookup_table(void) {
+  for(bitboard position = 1; position > 0; position <<= 1) {
+    knight_move_lookup_table[position] = KNIGHT_NW_JUMP(position) | KNIGHT_NE_JUMP(position) | KNIGHT_SW_JUMP(position) | KNIGHT_SE_JUMP(position);
+  }
+}
+
+void LookupTables::initialize_pawn_single_push_lookup_table(void) {
+  for(bitboard position = 1; position > 0; position <<= 1) {
+    pawn_single_push_lookup_table[position] = NORTH(position);
+  }
+}
+
+void LookupTables::initialize_pawn_double_push_lookup_table(void) {
+  for(bitboard position = 1; position > 0; position <<= 1) {
+    if(position >= 0x100 && position < 0x10000) {
+      pawn_double_push_lookup_table[position] = NORTH(NORTH(position));
+    } else {
+      pawn_double_push_lookup_table[position] = 0;
+    }
+  }
+}
+
+void LookupTables::initialize_pawn_attack_lookup_table(void) {
+  for(bitboard position = 1; position > 0; position <<= 1) {
+    pawn_attack_lookup_table[position] = NORTHWEST(position) | NORTHEAST(position);
   }
 }
 
--- LookupTables.hpp
+++ LookupTables.hpp
@@ -11,11 +11,23 @@ public:
 
   // Getters
   bitboard get_king_move_bitboard(bitboard position);
+  bitboard get_knight_move_bitboard(bitboard position);
+  bitboard get_pawn_single_push_bitboard(bitboard position);
+  bitboard get_pawn_double_push_bitboard(bitboard position);
+  bitboard get_pawn_attack_bitboard(bitboard position);
 
 private:
   // Lookup Tables:
   std::map<bitboard, bitboard> king_move_lookup_table;
-
+  std::map<bitboard, bitboard> knight_move_lookup_table;
+  std::map<bitboard, bitboard> pawn_single_push_lookup_table;
+  std::map<bitboard, bitboard> pawn_double_push_lookup_table;
+  std::map<bitboard, bitboard> pawn_attack_lookup_table;
+  
   // Initializers:
   void initialize_king_move_lookup_table(void);
+  void initialize_knight_move_lookup_table(void);
+  void initialize_pawn_single_push_lookup_table(void);
+  void initialize_pawn_double_push_lookup_table(void);
+  void initialize_pawn_attack_lookup_table(void);
 };
